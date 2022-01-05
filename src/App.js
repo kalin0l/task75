fea25c6f3af7c8eddec0296c0523da4cefa0fdc4
kalin0l <aleksiev2494@gmail.com>
2022-01-05 17:31:32 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [text,setText] = useState('')
-  const saveToLocalStorage = () => {
-    localStorage.setItem('saved',text);
-  }
-  const clear =() => {
+  const [text, setText] = useState("");
+  const saveToLocalStorage = (e) => {
+    localStorage.setItem("saved", JSON.stringify(text))
+    
+  };
+  const clear = () => {
     localStorage.clear();
+  };
+ 
 
-  }
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" value={text} onChange={(e) => setText(e.target.value)} placeholder="Notes..." />
+            <textarea
+              className="textarea is-large"
+              value={localStorage.getItem('saved')}
+              onChange={(e) => setText(e.target.value)}
+              placeholder="Notes..."
+            />
           </div>
         </div>
-        <button className="button is-large is-primary is-active" onClick={saveToLocalStorage}>Save</button>
-        <button className="button is-large" onClick={clear}>Clear</button>
+        <button
+          className="button is-large is-primary is-active"
+          onClick={saveToLocalStorage}
+        >
+          Save
+        </button>
+        <button className="button is-large" onClick={clear}>
+          Clear
+        </button>
       </div>
     </div>
   );
